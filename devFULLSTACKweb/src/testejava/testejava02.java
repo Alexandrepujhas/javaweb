e6338ef7cdfb5d11aca49a1a23ff6ccc9c9369c4
Alexandrepujhas <alexandremaciell64@gmail.com>
2024-12-10 00:25:00 -0400

    teste /3

--- devFULLSTACKweb/src/testejava/testejava02.java
+++ devFULLSTACKweb/src/testejava/testejava02.java
@@ -2,7 +2,7 @@ package testejava;
 
 public class testejava02 {
 	public static void main(String[] args) {
-		System.out.print("teste");
+		System.out.print("teste, ve se foi o novo pull e commit");
 	}
 
 }
