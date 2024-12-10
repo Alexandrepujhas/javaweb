5612689738c5e48cb48364d81be20c41b40aca0f
Alexandrepujhas <alexandremaciell64@gmail.com>
2024-12-10 11:18:46 -0400

    commit test

--- devFULLSTACKweb/src/testejava/testejava02.java
+++ devFULLSTACKweb/src/testejava/testejava02.java
@@ -2,7 +2,7 @@ package testejava;
 
 public class testejava02 {
 	public static void main(String[] args) {
-		System.out.print("teste, ve se foi o novo pull e commit da chave publica");
+		System.out.print("teste, ve se foi o novo pull e commit da chave publica aa aaaaaaaa");
 	}
 
 }
